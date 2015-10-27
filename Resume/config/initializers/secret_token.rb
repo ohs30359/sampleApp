58bd5dd0f39657d4598b7eaf0d5a3620fc3d2ed0
ohs30359 <ohs30359@gmail.com>
2015-10-27 22:13:09 +0900

    環境構築完了

---
+++ Resume/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resume::Application.config.secret_key_base = 'd45796bc76d6d8a09ce0cbbca5ad5a39020c1956bc5c8deeedfe2395a49f48ed705de64053cc15663c08e5f5c36fc88d7ea0ffa7284b80d82711c4eceecef7a6'
